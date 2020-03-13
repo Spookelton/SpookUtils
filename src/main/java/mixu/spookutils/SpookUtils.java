8701d03d59ea8375dd05e90bd21c64b659730295
Mixu <hoppandebananen123@gmail.com>
2020-03-13 16:56:59 +0200

    Make mod not required on client to connect to server

--- src/main/java/mixu/spookutils/SpookUtils.java
+++ src/main/java/mixu/spookutils/SpookUtils.java
@@ -3,6 +3,11 @@ package mixu.spookutils;
 import mixu.spookutils.packet.NetworkHandler;
 import mixu.spookutils.proxy.CommonProxy;
 import mixu.spookutils.commands.reDumpDimensions;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -10,14 +15,17 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.Display;
 
-@Mod(modid = SpookUtils.MODID, name = SpookUtils.NAME, version = SpookUtils.VERSION)
+@Mod(modid = SpookUtils.MODID, name = SpookUtils.NAME, version = SpookUtils.VERSION, acceptableRemoteVersions = "*")
 public class SpookUtils
 {
     public static final String MODID = "spookutils";
     public static final String NAME = "SpookUtils";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
 
     @SidedProxy(clientSide = "mixu.spookutils.proxy.ClientProxy", serverSide = "mixu.spookutils.proxy.ServerProxy")
     public static CommonProxy proxy;
